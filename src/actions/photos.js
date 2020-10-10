fc955dbb24626b61068dcd101319c85e4fba7cea
JaronArmiger <jaron.armiger@gmail.com>
2020-10-10 18:19:07 -0500

    write photos action

---
+++ src/actions/photos.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { BASE_API_URL } from '../utils/constants';
+import { getErrors } from './errors';
+
+export const beginAddPhoto = (photo) => {
+  return async (dispatch) => {
+    try {
+      const formData = new FormData();
+      formData.append('photo', photo);
+      await acios.post(`${BASE_API_URL}/photos`, formData, {
+      	headers: {
+      	  'Content-Type': 'multipart/form-data',
+      	}
+      });
+    } catch (err) {
+      err.response && dispatch(getErrors(err.response.data));
+    }
+  };
+};
+
+export const startLoadPhotos = () => {
+  return async (dispatch) => {
+    try {
+      const photos = await axios.get(`${BASE_API_URL}/photos`);
+      dispatch(loadPhotos(photos.data));
+    } catch (err) {
+      err.response && dispatch(getErrors(err.response.data));
+    }
+  };
+};
+
+export const loadPhotos = (photos) => ({
+  type: 'LOAD_PHOTOS',
+  photos
+});
