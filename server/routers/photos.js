e80c56f8b577607707d5d18c87e186c4b5babd24
JaronArmiger <jaron.armiger@gmail.com>
2020-10-10 19:11:41 -0500

    write server/routers/photos.js

---
+++ server/routers/photos.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const multer = require('multer');
+const Photo = require('../model/Photo');
+const Router = express.Router();
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000, //max file size 1MB = 1000000 bytes
+  },
+  fileFilter(req, file, cb) {
+  	if (!file.originalname.match(/\.(jpeg|jpg)$/)) {
+  	  cb(new Error('only upload files with jpg or jpeg format.'));
+  	}
+  	cb(undefined, true); // continue with the upload
+  }
+});
+
+Router.post('/photos', upload.single('photo'), async (req, res) => {
+    try {
+      const photo = new Photo(req.body);
+      const file = req.file.buffer;
+      photo.photo = file;
+
+      await photo.save();
+      res.status(201).send({ _id: photo._id });
+    } catch (err) {
+      res.status(500).send({
+        upload_error: 'Error while uploading file..',
+      });
+    }
+  }, (err, req, res, next) => {
+  	if (err) {
+  	  res.status(500).send({
+  	    upload_error: err.message,
+  	  });
+  	}
+});
+
+Router.get('/photos', async (req, res) => {
+  try {
+    const photos = await Photo.find({});
+    res.send(photos);
+  } catch (err) {
+    res.status(500).send({ get_error: 'Error while getting list of photos' });
+  }
+});
+
+Router.get('/photos/:id', async (req, res) => {
+  try {
+    const result = await Photo.findById(req.params.id);
+    res.set('Content-Type', 'image/jpeg');
+    res.send(result.photo);
+  } catch (error) {
+    res.status(400).send({ get_error: 'Error while getting photo' });
+  }
+});
+
+module.exports = Router;
