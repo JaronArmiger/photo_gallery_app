2d715498dadca2e0c7c19997312c8ba880b0e156
JaronArmiger <jaron.armiger@gmail.com>
2020-10-12 16:05:15 -0500

    change arrow function in PhotoSchema toJSON method to regular function (no longer causes error)

--- server/model/Photo.js
+++ server/model/Photo.js
@@ -5,13 +5,13 @@ const PhotoSchema = mongoose.Schema({
     type: Buffer,
   }
 });
-/*
-PhotoSchema.methods.toJSON = () => {
+
+PhotoSchema.methods.toJSON = function() {
   const result = this.toObject();
   delete result.photo;
   return result;
 };
-*/
+
 const Photo = mongoose.model('Photo', PhotoSchema);
 
 module.exports = Photo;
