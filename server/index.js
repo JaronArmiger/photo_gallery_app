0c4eb585b1c4a50f26f79726e51dd1271a4fb79e
JaronArmiger <jaron.armiger@gmail.com>
2020-10-12 16:17:12 -0500

    add middleware that sends the react app's index.html after photosRouter

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 3300;
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(photosRouter);
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
