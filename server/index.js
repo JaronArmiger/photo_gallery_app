f300d562d071f3397d610017314492b8a06bc566
JaronArmiger <jaron.armiger@gmail.com>
2020-10-12 16:10:11 -0500

    remove base url from constants.js and remove use of cors from server/index.js, instead use express.static and serve from build folder

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
+//const cors = require('cors');
 const photosRouter = require('./routers/photos');
 require('./db/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3300;
 
-app.use(cors());
+//app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(photosRouter);
 
 app.listen(PORT, () => {
